990171b7e353c6270e4477bd62c92cee2dd5863f
AbdulmalikAbdullah <abdulmalik.alabdulwahab@gmail.com>
2025-12-05 21:06:33 +0300

    fix members component

--- frontend/src/components/MembersList/MembersList.jsx
+++ frontend/src/components/MembersList/MembersList.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import './MembersList.css';
+import '../Shared/Modal.css';
 
 export default function MembersList({ members }) {
+  const emailIcon = "../../../assets/emailIcon.svg";
   return (
     <div className="members-panel">
       {members.length === 0 ? (
-        <div>No members</div>
+        <div className='members-message'>No members</div>
       ) : (
         members.map((m) => (
-          <div key={m._id} className="member-row" onClick={() => onMemberClick && onMemberClick(m)}>
-            {m.displayName || m.email}
+          <div key={m._id} className="member-col" onClick={() => onMemberClick && onMemberClick(m)}>
+            <p className='name'>{`${m.firstName} ${m.lastName}`} </p>
+            <p className='email'>{m.email}</p>
+            <a href={`mailto:${m.email}`} target='_blank' className="btn primary">
+              Send Email <img className='email-icon' src={emailIcon} alt="email icon" />
+            </a>
           </div>
         ))
       )}
