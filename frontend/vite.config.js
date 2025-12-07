70394ab01386d5f905e004fb8b863cb0b1b52a29
AbdulmalikAbdullah <abdulmalik.alabdulwahab@gmail.com>
2025-12-07 16:51:05 +0300

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  // Optional but good to have:
-  base:"/dashboard",
+  base: '/dashboard/', // Add trailing slash
   build: {
     outDir: 'dist',
-    copyPublicDir: true  // This ensures files from public/ are copied
+    copyPublicDir: true
   }
 })
