0b715324ac95f452afb23712aad8ad44cb909998
AbdulmalikAbdullah <abdulmalik.alabdulwahab@gmail.com>
2025-12-08 22:12:35 +0300

    fix api link for forget function

--- frontend/src/components/ForgetPassword/ForgetPassword.jsx
+++ frontend/src/components/ForgetPassword/ForgetPassword.jsx
@@ -14,7 +14,7 @@ const ForgotPassword = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post(`${BACKEND_URL}api/auth/forgot-password`, { email });
+      await axios.post(`${BACKEND_URL}/api/auth/forgot-password`, { email });
       setShowNotifsuccess(true);
       setTimeout(() => {
         setShowNotifsuccess(false);
